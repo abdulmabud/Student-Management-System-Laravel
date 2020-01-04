a19c19f80302eb45566f4c4b1ad48b2981f186f4
abdulmabud <abdulmabudm@gmail.com>
2020-01-04 20:21:40 +0600

    Users table seeder added

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(5).'gmail.com',
+            'password' => Hash::make(123456)
+        ]);
+    }
+}
